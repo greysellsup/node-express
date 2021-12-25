0ca46e7a8af93c83ec0dd4f115ba25fb34fc6d4d
grsl <greysellsup@gmail.com>
2021-12-25 10:29:58 +0500

    init project

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-start
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on pont: ${PORT}`);
+})
