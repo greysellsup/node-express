99d43fba190ad3b0bcfa4bb156ad446a5330695a
grsl <grsl@skbkontur.ru>
2021-12-29 00:00:35 +0500

    add routes

--- index.js
+++ index.js
@@ -1,42 +1,28 @@
 const express = require('express')
-const path = require('path')
 const app = express()
 const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const gamesRoutes = require('./routes/games')
 
+//подключили handlebars
 const hbs =  exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
 })
-
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+// подключили статику (css)
 app.use(express.static('public'))
 
-app.get('/',(req, res)=> {
-    res.render('index',{
-        title: 'Home',
-        isHome: true
-    })
-})
-
-app.get('/add',(req, res)=> {
-    res.render('add',{
-        title: 'Add game',
-        isAdd: true
-    })
-})
-
-app.get('/games',(req, res)=> {
-    res.render('games',{
-        title: 'Games',
-        isGames: true
-    })
-})
+// подключили роуты
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/games', gamesRoutes)
 
 const PORT = process.env.PORT || 3000
-
 app.listen(PORT, () =>{
     console.log(`Server is running on pont: ${PORT}`);
 })
