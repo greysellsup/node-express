483ad3a2d3f5b0648b5a45a13de873be16674660
grsl <grsl@skbkontur.ru>
2022-01-07 18:47:24 +0500

    add html games template

--- routes/game.js
+++ routes/game.js
@@ -1,10 +1,14 @@
 const {Router} =  require('express')
+const Game = require('../models/game')
 const router = Router()
 
-router.get('/',(req, res) =>{
+router.get('/',async (req, res) =>{
+    const games =  await Game.getAll();
+    console.log(games);
     res.render('games',{
         title: 'Games',
-        isGames: true
+        isGames: true,
+        games
     })
 })
 
