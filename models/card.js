8ae53c47e29d173522d80f0c4cc2a5e3165babd9
grsl <grsl@skbkontur.ru>
2022-01-08 18:22:33 +0500

    add games in card

--- models/card.js
+++ models/card.js
@@ -1,12 +1,47 @@
 const fs = require('fs')
 const path = require('path')
 
-class Card {
-    constructor() {
+//путь до файла корзины
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'card.json'
+)
 
+class Card {
+  static async add(game) {
+    const card = await Card.fetch(); // получаем игры из корзины
+    const index = card.games.findIndex(item => item.id === game.id); //Находим индекс игры если она есть уже есть в корзине
+    const selectedGame = card.games[index];
+
+    if(selectedGame){
+      //игра уже в корзине
+      selectedGame.count++;
+      card.games[index] = selectedGame;
+    } else {
+      //добавляем игру
+      game.count = 1;
+      card.games.push(game)
     }
 
-
+    card.price += +game.price; //сумму стоимости игр
+    
+    //Записываем добавленную в корзину игру в JSON
+    return new Promise((resolve, reject)=>{
+      fs.writeFile(p, JSON.stringify(card), err => {
+        err ? reject(err) : resolve()
+      })
+    })
+  }
+  
+  //получаем данные о играх в корзине
+  static async fetch(){
+    return new Promise((resolve, reject)=>{
+      fs.readFile(p, 'utf-8',(err, content)=>{
+        err ? reject(err) : resolve(JSON.parse(content))
+      })
+    })
+  }
 }
 
 module.exports = Card
--- routes/card.js
+++ routes/card.js
@@ -6,16 +6,19 @@ const Game = require('../models/game')
 //получаем POST при добавлении в корзину и переходим в корзину
 router.post('/add', async (req, res) => {
     const game = await Game.getById(req.body.id)
+    console.log(game);
     await Card.add(game)
     res.redirect('/card')
 })
 
 //обрабатываем роут выводим данные
 router.get('/', async (req,res) =>{
-    const game = await Card.fetch();
+    const card = await Card.fetch();
     res.render('card',{
         tittle: 'Card',
-        card
+        isCard: true,
+        games: card.games,
+        price: card.price
     })
 })
 
